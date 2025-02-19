f789c61a643653a8a703f6be9e91c556b9d080ba
Philipp Renz <renz.ph@gmail.com>
2025-02-19 09:39:49 +0100

    new install script and remove tmux plugins

--- .zshrc
+++ .zshrc
@@ -15,8 +15,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time Oh My Zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-# ZSH_THEME="robbyrussell"
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="robbyrussell"
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
 # a theme from this variable instead of looking in $ZSH/themes/
@@ -77,7 +76,7 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-syntax-highlighting autoenv)
+plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -118,5 +117,3 @@ source $ZSH/oh-my-zsh.sh
 
 source $HOME/.cargo/env
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+if [ -d "$HOME/.oh-my-zsh/" ]; then
+    echo "oh-my-zsh is already installed as ~/.oh-my-zsh already exists"
+else
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "Hithere"
+    rm -f "$HOME/.zshrc"
+fi
+
+
+# Define repositories as tuples (repo_url destination)
+repos=(
+    "https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    "https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    "https://github.com/zpm-zsh/autoenv $HOME/.oh-my-zsh/custom/plugins/autoenv"
+    "https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm"
+    "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+)
+
+
+# Output directory for fonts
+OUTPUT_DIR="$HOME/.local/share/fonts"
+
+# Function to clone repositories gracefully
+clone_repo() {
+    repo_url=$1
+    target_dir=$2
+
+    if [ -d "$target_dir" ]; then
+        echo "Skipping: '$target_dir' already exists."
+    else
+        echo "Cloning: $repo_url -> $target_dir"
+        git clone "$repo_url" "$target_dir" 2>&1 | sed 's/^fatal: //'
+    fi
+}
+
+# Iterate over repositories
+for repo in "${repos[@]}"; do
+    clone_repo $repo
+done
+
+
+# Output directory for fonts
+OUTPUT_DIR="$HOME/.local/share/fonts"
+mkdir -p "$OUTPUT_DIR"
+
+# Font URLs and corresponding filenames
+fonts=(
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf" "MesloLGS NF Regular.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf" "MesloLGS NF Bold.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf" "MesloLGS NF Italic.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf" "MesloLGS NF Bold Italic.ttf"
+)
+
+download_font() {
+    url="$1"
+    filename="$2"
+    filepath="$OUTPUT_DIR/$filename"
+
+    if [ -f "$filepath" ]; then
+        echo "Skipping: '$filename' already exists."
+    else
+        echo "Downloading: $filename"
+        curl -o "$filepath" "$url" --fail --silent || echo "Failed to download: $filename"
+    fi
+}
+
+# Iterate over font URLs and filenames (handling spaces correctly)
+for ((i=0; i<${#fonts[@]}; i+=2)); do
+    download_font "${fonts[i]}" "${fonts[i+1]}"
+done
+
+
+stow . 
